b88a5c424d760bd89406204548e0211c0d000cd2
Andrii Tkach <andrii@gezondmetsalut.nl>
2023-03-16 13:20:32 +0100

    More components

--- SalutRNW/common/src/index.tsx
+++ SalutRNW/common/src/index.tsx
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
-import { Button, SafeAreaView, ScrollView, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { SafeAreaView, ScrollView, View, StyleSheet } from 'react-native';
+import { Counter } from './screens/Counter';
+import { Router } from './Router';
+import { Card } from './components/Card';
 
 export const App = () => {
-	const [count, setCount] = useState(0);
-
 	return (
 		<SafeAreaView>
 			<ScrollView contentInsetAdjustmentBehavior='automatic'>
-				<View>
-					<View>
-						<Text>Hello {count}</Text>
-					</View>
-					<Button
-						title='Increment'
-						onPress={() =>
-							setCount(count + 1)
-						}
-					/>
+				<View style={styles.container}>
+					<Counter />
+          <Router />
+          <Card text='Test card'/>
 				</View>
 			</ScrollView>
 		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'pink',
+	},
+});
