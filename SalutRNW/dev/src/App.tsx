abe1bd541ae2efc7d0fe12713437903e419d8bc6
Andrii Tkach <andrii@gezondmetsalut.nl>
2023-03-23 17:27:45 +0100

    Add dashboard file to app

--- SalutRNW/dev/src/App.tsx
+++ SalutRNW/dev/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import { Card } from './salut-lib/components/Card';
+import { Dashboard } from "./Dashboard";
 
 interface Props {}
 
@@ -11,6 +12,7 @@ export const App: React.FC<Props> = () => {
 				<Text>Salut dev env: 0.1</Text>
 			</View>
 			<Card text='Test card' onButtonClick={(result) => console.log('test card click, result:', result)}></Card>
+      <Dashboard />;
 		</View>
 	);
 };
